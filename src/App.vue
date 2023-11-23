2a556995850d6290b7e84e4bbec40dfe175c131e
Almat Zhanabekov <a.zhanabekov@acepas.kz>
2023-11-23 10:41:43 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <UserForm @form-submitted="updateUserInfo" />
+    <UserInfo :user="userInfo" />
+  </div>
+</template>
+
+<script>
+import UserForm from "@/components/UserForm.vue";
+import UserInfo from "@/components/UserInfo.vue";
+
+export default {
+  components: {
+    UserForm,
+    UserInfo,
+  },
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+  methods: {
+    updateUserInfo(user) {
+      console.log(user);
+      this.userInfo = user;
+    },
+  },
+};
+</script>
+
+<style>
+/* Ваши глобальные стили */
+body {
+  font-family: "Arial", sans-serif;
+  background-color: #f4f4f4;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app {
+  text-align: center;
+}
+
+.user-form {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+}
+
+.user-info {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="user-info">
+      <h2>Иинформация о пользователе</h2>
+        <p>Имя: {{ user.name }}</p>
+        <p>Телефон: {{ user.phone }}</p>
+        <p>Возраст: {{ user.age }}</p>
+        <p>Дата рождения: {{ user.birthday }}</p>
+        <p>Количество детей: {{ user.children }}</p>
+        
+        <!-- Отображение остальных полей -->
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+    p{
+        margin: 20px;
+    }
+  </style>
+  
